0ccca3c0d8f9f598f53868e640b483363fe1307f
ISNewton <ashrfalhag.11@gmail.com>
2024-02-24 09:37:30 +0200

    feat:handle non-404 errors

--- src/helpers/videoUploader.ts
+++ src/helpers/videoUploader.ts
@@ -1,34 +1,36 @@
-import axios from 'axios';
-import FormData from 'form-data';
-import { createReadStream, fstat } from 'fs';
-import fs from 'fs'
-import { Context } from 'telegraf';
+import axios from "axios";
+import FormData from "form-data";
+import { createReadStream, fstat } from "fs";
+import fs from "fs";
+import { Context } from "telegraf";
 
 export async function sendLargeDocument(
   ctx: Context,
-  path: string,
+  path: string
 ): Promise<any> {
-
   const totalSize = await fs.promises.stat(path).then((stats) => stats.size);
 
   const formData = new FormData();
-  formData.append('chat_id', ctx.chat?.id);
-  formData.append('document', createReadStream(path));
-  formData.append('supports_streaming', 'true');
-  return await axios
-    .post(
-      `http://127.0.0.1:8081/bot${process.env.BOT_TOKEN}/sendDocument`,
-      formData,
-      {
-        headers: { 'Content-Type': 'multipart/form-data' },
-        maxContentLength : Infinity,
-        maxBodyLength : Infinity,
-         onUploadProgress: (progressEvent) => {
+  formData.append("chat_id", ctx.chat?.id);
+  formData.append("document", createReadStream(path));
+  formData.append("supports_streaming", "true");
+  return await axios.post(
+    `http://127.0.0.1:8081/bot${process.env.BOT_TOKEN}/sendDocument`,
+    formData,
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
+      validateStatus: (status) => status < 500, // Handle non-400 errors
+      onUploadProgress: (progressEvent) => {
         // Optional: Track upload progress and report to user
         const percentage = Math.round((progressEvent.loaded * 100) / totalSize);
 
-        ctx.reply(`Upload progress: ${percentage}%`)
+        ctx.reply(`
+                  Downloded : ${progressEvent.loaded}
+                  Upload progress: ${percentage}%
+                  `);
       },
-      },
-    )
+    }
+  );
 }
